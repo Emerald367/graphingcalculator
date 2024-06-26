af95334393812b30ce37a1424001d9e695a39afb
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-06-26 14:52:18 -0700

    created a homepage and installed react router

--- graphing-calculator/src/App.js
+++ graphing-calculator/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
+import LoginForm from './LoginForm';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <SignUpForm />
+      <nav className="bg-green-600 p-4">
+        <ul className="flex space-x-4 justify-center">
+          <li>
+            <Link to="/login" className="text-white">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup" className="text-white">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
