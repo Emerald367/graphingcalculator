e84eca69527ad37194cc3061331610eaba482555
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-07-16 14:48:00 -0700

    requests for equations without a user

--- server.js
+++ server.js
@@ -191,113 +191,6 @@ app.get('/users', verifyToken, async (req, res) => {
      }
 });
 
-app.get('/users/settings', verifyToken, async (req, res) => {
-   const userId = req.user.userId;
-   
-   try {
-      const { data, error } = await supabase
-          .from('usersettings')
-          .select('*')
-          .eq('user_id', userId)
-          .single();
-
-       if (error) {
-           console.error('Error fetching user settings:', error);
-           return res.status(500).json({ error: 'Error fetching user settings' });
-       }
-
-       res.status(200).json({ settings: data });
-   } catch (error) {
-       console.error('Unexpected error occurred:', error);
-       res.status(500).json({ error: 'Unexpected error occurred' })
-   }
-})
-
-app.post('/users/settings', verifyToken, async (req, res) => {
-   const userId = req.user.userId;
-   const { theme, grid_lines, axis_settings } = req.body;
-
-   if (typeof theme !== 'string' || typeof grid_lines !== 'boolean' || typeof axis_settings !== 'object') {
-      return res.status(400).json({ error: 'Invalid request body' });
-   }
-   
-   try {
-      const {data, error} = await supabase
-        .from('usersettings')
-        .insert([{ user_id: userId, theme, grid_lines, axis_settings }])
-        .select()
-        .single();
-   
-
-   if (error) {
-      console.error('Error inserting user settings:', error);
-      return res.status(500).json({ error: 'Error inserting user settings' });
-   }
-
-   res.status(201).json({ settings: data });
-  } catch (error) {
-     console.error('Unexpected error occurred:', error);
-     res.status(500).json({ error: 'Unexpected error occurred' });
-  }
-});
-
-app.put('/users/settings', verifyToken, async (req, res) => {
-   console.log('Request Body:', req.body);
-
-   const userId = req.user.userId;
-   const { theme, grid_lines, axis_settings } = req.body;
-
-   if (typeof theme !== 'string') {
-      console.error('Invalid theme:', theme);
-      return res.status(400).json({error : 'Invalid theme'});
-      }
-
-   if (typeof grid_lines !== 'boolean') {
-      console.error('Invalid grid_lines:', grid_lines);
-      return res.status(400).json({ error: 'Invalid grid_lines' });
-   }
-
-   if (typeof axis_settings !== 'object') {
-      console.error('Invalid axis_settings:', axis_settings);
-      return res.status(400).json({ error: 'Invalid axis_settings' })
-   }
-
-   try {
-      const { data: existingSettings, error: fetchError } = await supabase
-        .from('usersettings')
-        .select('id')
-        .eq('user_id', userId)
-        .single();
-
-      if (fetchError) {
-         console.error('Error fetching user settings:', fetchError);
-         return res.status(500).json({ error: 'Error fetching user settings' });
-      }
-
-      if (!existingSettings) {
-         console.error('No settings found for user:', userId);
-         return res.status(404).json({ error: 'No settings found for user' });
-      }
-
-      const { data, error } = await supabase
-        .from('usersettings')
-        .update({ theme, grid_lines, axis_settings })
-        .eq('user_id', userId)
-        .eq('id', existingSettings.id)
-        .select()
-        .single();
-
-      if (error) {
-         console.error('Error updating user settings', error);
-         return res.status(500).json({ error: 'Error updating user settings' });
-      }
-
-      res.status(200).json({ settings: data });
-   } catch (error) {
-     console.error('Unexpected error occurred:', error);
-     res.status(500).json({ error: 'Unexpected error occurred' });
-   }
-});
 
 app.post('/graphs', verifyToken, async (req, res) => {
    const userId = req.user.userId;
@@ -476,7 +369,6 @@ const validateEquation = (equation) => {
    const trigonometricRegex = /^y\s*=\s*(sin|cos|tan)\(x\)\s*([+-]\s*\d+)?$/;
    const conicSectionsRegex = /^(\d*\s*x\^2\s*[+-]\s*\d*\s*y\^2\s*=\s*\d+)$/;
    const rationalRegex = /^y\s*=\s*\d*x\/\d+(\s*[+-]\s*\d+)?$/;
-   const radicalRegex = /^y\s*=\s*\d*\s*\*sqrt\(\d*x\)\s*([+-]\s*\d+)?$/;
    const circleRegex = /^\s*\(x\s*[+-]\s*\d+\)\^2\s*[+-]\s*\(y\s*[+-]\s*\d+\)\^2\s*=\s*\d+$/;
 
    const regexes = [
@@ -488,7 +380,6 @@ const validateEquation = (equation) => {
       trigonometricRegex,
       conicSectionsRegex,
       rationalRegex,
-      radicalRegex,
       circleRegex
    ]
 
@@ -544,6 +435,93 @@ app.delete('/graphs/equations/:id', verifyToken, async (req, res) => {
       }
 
       res.status(200).json({ message: 'Equation deleted successfully', equation: data })
+   } catch (error) {
+      console.error('Unexpected error occurred:', error);
+      res.status(500).json({ error: 'Unexpected error occurred' });
+   }
+});
+
+app.post('/public/graphs/equations', async (req, res) => {
+   const { equation, color, thickness } = req.body;
+
+   if (!equation || !color || thickness == null) {
+      return res.status(400).json({ error: 'Equation is required' });
+   }
+
+   if (!validateEquation(equation)) {
+      return res.status(400).json({ error: 'Invalid equation format' });
+   }
+
+   const id = uuidv4();
+
+   try {
+      const { data, error } = await supabase
+         .from('equations')
+         .insert([{ equation, color, id, thickness }])
+         .select()
+         .single();
+
+      if (error) {
+         console.error('Error adding equation:', error);
+         return res.status(500).json({ error: 'Error creating equation' });
+      }
+
+      res.status(201).json({ equation: data });
+
+   } catch (error) {
+      console.error('Unexpected error occurred:', error);
+      res.status(500).json({ error: 'Unexpected error occurred' });
+   }
+});
+
+app.put('/public/graphs/equations/:id', async (req, res) => {
+   const { id } = req.params;
+   const { equation, color, thickness } = req.body;
+
+   if (!equation || !color || thickness == null) {
+      return res.status(400).json({ error: 'Equation, color, and thickness are required' });
+   }
+
+   if (!validateEquation(equation)) {
+      return res.status(400).json({ error: 'Invalid equation format' });
+   }
+
+   try {
+      const { data, error } = await supabase
+         .from('equations')
+         .update({ equation, color, thickness })
+         .eq('id', id)
+         .select()
+         .single();
+
+      if (error) {
+         console.error('Error updating equation:', error);
+         return res.status(500).json({ error: 'Error updating equation' });
+      }
+
+      res.status(200).json({ equation: data });
+   } catch (error) {
+      console.error('Unexpected error occurred:', error);
+      res.status(500).json({ error: 'Unexpected error occurred' });
+   }
+});
+
+app.delete('/public/graphs/equations/:id', async (req, res) => {
+   const { id } = req.params;
+   console.log('Delete request received for ID:', id);
+
+   try {
+      const { data, error } = await supabase
+         .from('equations')
+         .delete()
+         .eq('id', id);
+
+      if (error) {
+         console.error('Error deleting equation:', error);
+         return res.status(500).json({ error: 'Error deleting equation' });
+      }
+
+      res.status(200).json({ message: 'Equation deleted successfully', equation: data });
    } catch (error) {
       console.error('Unexpected error occurred:', error);
       res.status(500).json({ error: 'Unexpected error occurred' });
